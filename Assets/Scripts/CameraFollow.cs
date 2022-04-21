2335b6d684994fddf8a90a47e2b88cc0601c2750
Sintonix <kelvinbly01@gmail.com>
2022-04-21 01:20:30 -0700

    2

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,13 +4,45 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform target;
-    public Vector3 offset; 
+    [SerializeField]
+    private float mouseSensitivity = 3.0f;
 
+    private float rotationY;
+    private float rotationX;
 
-    // Update is called once per frame
+    [SerializeField]
+    private Transform target;
+
+    [SerializeField]
+    private float distanceFromTarget = 3.0f;
+
+    private Vector3 currentRotation;
+    private Vector3 smoothVelocity = Vector3.zero;
+
+    [SerializeField]
+    private float smoothTime = 0.2f;
+
+    [SerializeField]
+    private Vector2 _rotationXMinMax = new Vector2(-40, 40);
+
     void Update()
     {
-        transform.position = target.position + offset;
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        rotationY += mouseX;
+        rotationX += mouseY;
+
+        // Apply clamping for x rotation 
+        rotationX = Mathf.Clamp(rotationX, _rotationXMinMax.x, _rotationXMinMax.y);
+
+        Vector3 nextRotation = new Vector3(rotationX, rotationY);
+
+        // Apply damping between rotation changes
+        currentRotation = Vector3.SmoothDamp(currentRotation, nextRotation, ref smoothVelocity, smoothTime);
+        transform.localEulerAngles = currentRotation;
+
+        // Substract forward vector of the GameObject to point its forward vector to the target
+        transform.position = target.position - transform.forward * distanceFromTarget;
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,29 +4,42 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private CharacterController controller;
 
-    //Inputs
-    private float speed = 20.0f;
-    private float turnSpeed = 20.0f;
+    [SerializeField]
+    private float playerSpeed = 5.0f;
 
-    private float horizontalInput;
-    private float verticalInput;
+    [SerializeField]
+    private float rotationSpeed = 10.0f;
 
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private Camera followCamera;
+
+    private void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
+        Movement();
+    }
 
-        //Vehicle Turning and Controls
-        Debug.Log(Time.deltaTime);
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+    void Movement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movementInput = Quaternion.Euler(0, followCamera.transform.eulerAngles.y, 0) * new Vector3(horizontalInput, 0, verticalInput);
+        Vector3 movementDirection = movementInput.normalized;
+
+        if(movementDirection != Vector3.zero)
+        {
+            Quaternion desiredRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, desiredRotation, rotationSpeed * Time.deltaTime);
+        }
+
+        controller.Move(movementDirection * playerSpeed * Time.deltaTime);
     }
 }
