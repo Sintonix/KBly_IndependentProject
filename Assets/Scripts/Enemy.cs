190a97a083260b2121f58f235ac4ae809734209e
Sintonix <kelvinbly01@gmail.com>
2022-05-06 18:23:21 -0700

    fdsz

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    Rigidbody enemyRB;
+    GameObject player;
+    public float speed = 4.0f;
+
+    private float yBoundary = -15.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        enemyRB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y < yBoundary)
+        {
+            Destroy(gameObject);
+        }
+        Vector3 seekDirection = (player.transform.position - transform.position).normalized;
+        enemyRB.AddForce( seekDirection * speed * Time.deltaTime);
+    }
+}
