87d401483a4902b2572ff888cecf21f5f48ab784
Sintonix <kelvinbly01@gmail.com>
2022-05-21 18:13:19 -0700

    uygthfv

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public List<GameObject> prefabs;
+
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public Button restartButton;
+    public GameObject titleScreen;
+
+    private int score = 0;
+    public bool gameActive = false;
+
+    public void StartGame(int diff)
     {
-        
+        gameActive = true;
+        score = 0;
+        UpdateScore(0);
+        titleScreen.gameObject.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void GameOver()
     {
-        
+        gameActive = false;
+        gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+    }
+
+    public void UpdateScore(int ScoreDelta)
+    {
+        score += ScoreDelta;
+        if (score < 0)
+        {
+            score = 0;
+        }
+        scoreText.text = "Score: " + score;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
